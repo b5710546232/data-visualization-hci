6fc2a5c1c364c6d4b901a42fc7f72af436ac75ac
b5710546232 <nattapat.su@ku.th>
2016-12-02 09:47:10 +0700

    add data to line

--- src/assets/read.py
+++ src/assets/read.py
@@ -2,5 +2,5 @@ import csv
 f = open('year_cat_rate.csv','r')
 reader = f.read().splitlines()
 for r in reader:
-    if(r.split(',')[1]==' Romance '):
+    if(r.split(',')[1]==' Western '):
         print('%s,'%(r.split(',')[2]))
